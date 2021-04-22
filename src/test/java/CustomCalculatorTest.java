2b5026df41eb4693c93fbf2deb603bd0a07fce22
ShinYooJung <shinyoojung0@gmail.com>
2021-04-22 22:47:49 +0900

    Software Testing 1주차 과제

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -11,37 +11,37 @@ public class CustomCalculatorTest {
 
     //더하기 테스트 작성
     @Test
-    public void add() {
+    public void addTest() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        int result = customCalculator.add(10,10);
+        assertTrue(result == 20);
         System.out.println("result :: " + result);
     }
 
     //빼기 테스트 작성
     @Test
-    public void subtract() {
+    public void subtractTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 13);
         System.out.println("result :: " + result);
     }
 
     //곱하기 테스트 작성
     @Test
-    public void multiply() {
+    public void multiplyTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 45);
         System.out.println("result :: " + result);
     }
 
     //나누기 테스트 작성
     @Test
-    public void divide() {
+    public void divideTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 5);
         System.out.println("result :: " + result);
     }
 }
